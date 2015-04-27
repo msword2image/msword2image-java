09a7f911312e34a435bf6609d757700f837b9d04
msword2image <support@msword2image.com>
2015-04-27 15:21:57 -0400

    Update Input.java

--- src/Input.java
+++ src/Input.java
@@ -1,4 +1,19 @@
 package com.mswordtoimage;
 
 public class Input {
+  private InputType type;
+  private String value;
+  
+  public Input(InputType type, String value) {
+    this.type = type;
+    this.value = value;
+  }
+  
+  public InputType getType() {
+    return this.type;
+  }
+  
+  public String getValue() {
+    return this.value;
+  }
 }
