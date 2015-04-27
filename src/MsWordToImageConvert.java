ee3b7e30188d7cac17911c9dfcfa23dcb4dece6b
Mert Degirmenci <mertdeg@gmail.com>
2015-04-27 13:04:40 -0400

    Checkin

--- src/MsWordToImageConvert.java
+++ src/MsWordToImageConvert.java
@@ -1,4 +1,16 @@
 package com.mswordtoimage;
 
-public class MsWordToImage {
+public class MsWordToImageConvert {
+
+    private final String apiUser;
+    private final String apiKey;
+    
+    /**
+     * @param apiUser The API username given from mswordtoimage.com
+     * @param apiKey  The API password
+     */
+    public MsWordToImageConvert(String apiUser, String apiKey) {
+        this.apiUser = apiUser;
+        this.apiKey  = apiKey;
+    }
 }
