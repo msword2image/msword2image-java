39c640d628b891747b91f1e16d72dbed6b29dec6
msword2image <support@msword2image.com>
2015-04-27 15:31:04 -0400

    Update Output.java

--- src/Output.java
+++ src/Output.java
@@ -1,4 +1,25 @@
 package com.mswordtoimage;
 
 public class Output {
+  private OutputType type;
+  private OutputImageFormat imageFormat;
+  private String value;
+  
+  public Output(OutputType type, OutputImageFormat imageFormat, String value) {
+    this.type = type;
+    this.imageFormat = imageFormat;
+    this.value = value;
+  }
+  
+  public OutputType getType() {
+    return this.type;
+  }
+  
+  public String getValue() {
+    return this.value;
+  }
+  
+  public OutputImageFormat getImageFormat() {
+    return this.imageFormat;
+  }
 }
